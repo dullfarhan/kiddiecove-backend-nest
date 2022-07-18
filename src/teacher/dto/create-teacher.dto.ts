a53d52c09cd4ca966ef337ad9ad0821350614eaf
dullfarhan <dullfarhan@gmail.com>
2022-07-18 15:09:55 +0500

    rough dto2

--- src/teacher/dto/create-teacher.dto.ts
+++ src/teacher/dto/create-teacher.dto.ts
@@ -1,9 +1,9 @@
-import { isString, } from 'class-validator';
+import { isString } from 'class-validator';
 import mongoose from 'mongoose';
 
 export class CreateTeacherDto {
-  _id: mongoose.Types.ObjectId;
-  
+  // _id: mongoose.Types.ObjectId;
+
   @isString()
   name: string;
   code: string;
