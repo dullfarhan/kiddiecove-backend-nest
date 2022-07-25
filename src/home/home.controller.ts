8d3e5aa33368e0c89feb40f5ee584ef3399ae9e1
Faisal <faisalsiddique10886@gmail.com>
2022-07-25 11:37:33 +0500

    api tag to home controller

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,9 +1,8 @@
-import {
-  Controller,
-  Post,
-} from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { HomeService } from './home.service';
 
+@ApiTags('Home')
 @Controller('home')
 export class HomeController {
   constructor(private readonly homeService: HomeService) {}
