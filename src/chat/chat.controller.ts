6746c4beb0f9c496465ad045fc06600f48510bbc
Faisal <faisalsiddique10886@gmail.com>
2022-08-15 17:17:36 +0500

    token generation bug fixed, user module fixed and chat module fixed

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body, Res } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Post, Body, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
+import { PermissionGuard } from 'src/Guard/permission.guard';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { GetChatDto } from './dto/get-chat.dto';
@@ -10,16 +12,25 @@ import { GetChatDto } from './dto/get-chat.dto';
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(PermissionGuard)
+  @UseGuards(AuthGuard('jwt'))
   @Post('/create')
   createChat(@Body() createChatDto: CreateChatDto, @Res() res: Response) {
     return this.chatService.createChat(createChatDto, res);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(PermissionGuard)
+  @UseGuards(AuthGuard('jwt'))
   @Post('/get')
   getChat(@Body() getChatDto: GetChatDto, @Res() res: Response) {
     return this.chatService.getChat(getChatDto, res);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(PermissionGuard)
+  @UseGuards(AuthGuard('jwt'))
   @Post('/get/recent')
   getRecentChat(@Body() getChatDto: GetChatDto, @Res() res: Response) {
     return this.chatService.getRecentChat(getChatDto, res);
